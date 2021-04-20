e6d3b7a070bd6feeb022e1af36a4a970de83fc46
pvlvsoft <pvlvsoft@gmail.com>
2021-04-20 22:06:36 +0200

    Initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p></p>
+ *
+ * <p></p>
+ *
+ * @author Vojtech Pavlu
+ * @version 2021-04-20
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(Main.class, args);
+    }
+}
